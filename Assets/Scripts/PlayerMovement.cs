f9e7622fa77a0ca03fa31a5e603f032fb1f7d434
Mvye <mervyn.mathew18@gmail.com>
2022-01-29 16:34:39 -0500

    Implemented smoother jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
     private Rigidbody2D body;
+    // For better jump
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    // For checking if player is grounded
     bool isGrounded = false;
     public Transform isGroundedChecker;
     public float checkGroundRadius;
@@ -21,6 +25,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Move();
         Jump();
+        BetterJump();
         CheckIfGrounded();
     }
     private void Move() {
@@ -29,6 +34,16 @@ public class PlayerMovement : MonoBehaviour
     private void Jump() {
         if (Input.GetKey(KeyCode.Space) && isGrounded)
             body.velocity = new Vector2(body.velocity.x, jumpForce);
+    }
+    // Credit to Board to Bits Games on youtube and his tutorial:
+    // "Better Jumping in Unity With Four Lines of Code"
+    private void BetterJump() {
+        if (body.velocity.y < 0) {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (body.velocity.y > 0 && !Input.GetKey(KeyCode.Space)) {
+            body.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
     }
     // Creates a circle at player's feet to check if player is on the ground
     private void CheckIfGrounded() {
