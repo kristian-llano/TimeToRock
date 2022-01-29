64170d2a54427a316955a036e6fefefc685374c9
KLLANO23 <llano.kristian@gmail.com>
2022-01-29 17:20:19 -0500

    Added player orientation to movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,7 +29,16 @@ public class PlayerMovement : MonoBehaviour
         CheckIfGrounded();
     }
     private void Move() {
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+        // Flips player when moving left-right
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
     }
     private void Jump() {
         if (Input.GetKey(KeyCode.Space) && isGrounded)
