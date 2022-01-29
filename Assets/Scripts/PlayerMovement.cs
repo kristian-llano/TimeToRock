b11e2b22ba973eebb3025bcdef96be2537655f24
Mvye <mervyn.mathew18@gmail.com>
2022-01-29 16:10:56 -0500

    Refactored movement into functions; fixed infinte jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
     private Rigidbody2D body;
+    bool isGrounded = false;
+    public Transform isGroundedChecker;
+    public float checkGroundRadius;
+    public LayerMask groundLayer;
 
     private void Awake()
     {
@@ -14,9 +19,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+    private void Move() {
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
-
-        if (Input.GetKey(KeyCode.Space))
-            body.velocity = new Vector2(body.velocity.x, speed);
+    }
+    private void Jump() {
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+            body.velocity = new Vector2(body.velocity.x, jumpForce);
+    }
+    // Creates a circle at player's feet to check if player is on the ground
+    private void CheckIfGrounded() {
+        Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
+        if (collider != null) {
+            isGrounded = true;
+        }
+        else {
+            isGrounded = false;
+        }
     }
 }
